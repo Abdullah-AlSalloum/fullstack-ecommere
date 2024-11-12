2c93ec83a0463a3306f0997e78b6ed06b0df1520
Abdullah <abdullahalsalloum06@gmail.com>
2024-11-13 00:35:43 +0300

    adding the submenu of the catagory sidebar and the title of the products

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,9 +1,29 @@
 import HomeBanner from "../../components/HomeBanner";
-
+import banner1 from '../../assets/images/banner1.png'
 function Home() {
   return (
     <>
       <HomeBanner />
+
+      <section className="homeProducts">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3">
+              <div className="banner">
+              <img src={banner1} className="cursor"/>
+              </div>
+            </div>
+            <div className="col-md-9 productRow">
+              <div className="d-flex align-items-center">
+                <div className="info w-75">
+                  <h3 className="mb-0 hd">BEST SELLERS</h3>
+                  <p className="text-light text-sml mb-0">Do not miss the current offers...</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
--- src/components/Header/Navigation/index.js
+++ src/components/Header/Navigation/index.js
@@ -3,6 +3,8 @@ import { IoIosMenu } from "react-icons/io";
 import { FaAngleDown } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import { FaAngleRight } from "react-icons/fa6";
+
 // import { CiHome } from "react-icons/ci";
 // import { GiClothes } from "react-icons/gi";
 // import { SiStmicroelectronics } from "react-icons/si";
@@ -22,15 +24,24 @@ function Navigation() {
                     <div className="row">
                         <div className="col-sm-2 navPart1">
                             <div className="catWrapper">
-                                <Button className="allCatTab align-items-center" onClick={()=>setIsOpenSideBarVal(!isOpenSideBarVal)}>
+                                <Button className="allCatTab align-items-center" onClick={() => setIsOpenSideBarVal(!isOpenSideBarVal)}>
                                     <span className="icon1 mr-2"><IoIosMenu /></span>
                                     <span className="text">ALL CATEGORIES</span>
                                     <span className="icon2 ml-2"><FaAngleDown /></span>
                                 </Button>
-                                <div className={`sidebarNav ${isOpenSideBarVal===true ? 'open' : ''}`}>
+                                <div className={`sidebarNav ${isOpenSideBarVal === true ? 'open' : ''}`}>
                                     <ul>
-                                        <li><Link to="/"><Button>Men</Button></Link></li>
-                                        <li><Link to="/"><Button>Women</Button></Link></li>
+                                        <li><Link to="/"><Button>Men <FaAngleRight className="ml-auto"/></Button></Link>
+                                            <div className="submenu">
+                                                <Link to="/"><Button>Clothes</Button></Link>
+                                                <Link to="/"><Button>Footwear</Button></Link>
+                                                <Link to="/"><Button>Watches</Button></Link>
+                                                <Link to="/"><Button>Clothes</Button></Link>
+                                                <Link to="/"><Button>Footwear</Button></Link>
+                                                <Link to="/"><Button>Watches</Button></Link>
+                                            </div>
+                                        </li>
+                                        <li><Link to="/"><Button>Women <FaAngleRight className="ml-auto"/></Button></Link></li>
                                         <li><Link to="/"><Button>Beauty</Button></Link></li>
                                         <li><Link to="/"><Button>Watches</Button></Link></li>
                                         <li><Link to="/"><Button>Kids</Button></Link></li>
