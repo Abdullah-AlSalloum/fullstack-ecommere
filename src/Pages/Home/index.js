b90f6fd4e486409426018a2a946ceb27fda0ea10
Abdullah <abdullahalsalloum06@gmail.com>
2024-11-20 19:05:09 +0300

    adding the footer section and the newsletter form

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -3,6 +3,7 @@ import banner1 from '../../assets/images/banner1.png';
 import banner2 from '../../assets/images/banner2.png';
 import banner3 from '../../assets/images/banner3.jpg';
 import banner4 from '../../assets/images/banner4.jpg';
+import newsLetterImg from '../../assets/images/coupon.png';
 import Button from "@mui/material/Button";
 import { IoIosArrowRoundForward } from "react-icons/io";
 import React from "react";
@@ -12,6 +13,8 @@ import 'swiper/css/navigation';
 import { Navigation } from "swiper/modules";
 import ProductItem from "../../components/ProductItem";
 import HomeCat from "../../components/HomeCat";
+import { IoMailOutline } from "react-icons/io5";
+
 
 
 
@@ -116,6 +119,28 @@ function Home() {
                 <img src={banner4} alt="banner4" className="cursor w-100" />
               </div>
               </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="newsLetterSection mt-3 mb-3 d-flex align-items-center">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <p className="text-white mb-1">$20 discount for your first order.</p>
+              <h3 className="text-white">join our newsletter and get...</h3>
+              <p className="text-light">Join our email subsecription now and get update on 
+                <br/>promotions and coupons.</p>
+
+                <form>
+                  <IoMailOutline />
+                  <input type="email" placeholder="Your Email Address"/>
+                  <Button>Sebsecribe</Button>
+                </form>
+            </div>
+            <div className="col-md-6">
+                <img src={newsLetterImg} alt="coupon"/>
             </div>
           </div>
         </div>
