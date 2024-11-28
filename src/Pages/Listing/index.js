1c6b5d471aaf23c3525758e72758d6c486437cd0
Abdullah <abdullahalsalloum06@gmail.com>
2024-11-28 15:46:57 +0300

    adding listing and the sidebar file

--- src/Pages/Listing/index.js
+++ src/Pages/Listing/index.js
@@ -1,22 +1,30 @@
+import { Link } from "react-router-dom";
 import SideBar from "../../components/SideBar";
+import { BiBorderRadius } from "react-icons/bi";
 
-function Listing(){
-    return(
-        <>
-        <section className="product_Listing_Page">
-            <div className="container">
-                <div className="productListing d-flex">
-                    <SideBar />
+function Listing() {
+  return (
+    <>
+      <section className="product_Listing_Page">
+        <div className="container">
+          <div className="productListing d-flex">
+            <SideBar />
 
-                    <div className="content_right">
-                        content_right
-                    </div>
-                </div>
+            <div className="content_right">
+              <Link to={"#"}>
+                <img
+                  src="https://res.cloudinary.com/dkgonwhvj/image/upload/v1731427470/1731427468095_New_Project_13.jpg"
+                  className="w-100"
+                  style={{borderRadius: "8px"}}
+                  alt=""
+                />
+              </Link>
             </div>
-        </section>
-        </>
-    )
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
-
 
 export default Listing;
--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,8 +1,114 @@
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import RangeSlider from 'react-range-slider-input';
+import 'react-range-slider-input/dist/style.css';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 function SideBar(){
+    const [value , setValue] = useState([100 , 60000]);
+    const [value2 , setValue2] = useState(0);
     return(
         <>
         <div className="sidebar">
-            SideBar
+            <div className="filterBox">
+                <h6>PRODUCT CATEGORIES</h6>
+                <div className='scroll'>
+                 <ul>
+                    <li>
+                    <FormControlLabel className='w-100' control={<Checkbox />} label="Men" />
+                    </li>
+                    <li>
+                    <FormControlLabel className='w-100' control={<Checkbox />} label="Women" />
+                    </li>
+                    <li>
+                    <FormControlLabel className='w-100' control={<Checkbox />} label="Beauty" />
+                    </li>
+                    <li>
+                    <FormControlLabel className='w-100' control={<Checkbox />} label="Kids" />
+                    </li>
+                    <li>
+                    <FormControlLabel className='w-100' control={<Checkbox />} label="Watched" />
+                    </li>
+                    <li>
+                    <FormControlLabel className='w-100' control={<Checkbox />} label="Gifts" />
+                    </li>
+                    <li>
+                    <FormControlLabel className='w-100' control={<Checkbox />} label="Men" />
+                    </li>
+                    <li>
+                    <FormControlLabel className='w-100' control={<Checkbox />} label="Women" />
+                    </li>
+                    <li>
+                    <FormControlLabel className='w-100' control={<Checkbox />} label="Beauty" />
+                    </li>
+                    <li>
+                    <FormControlLabel className='w-100' control={<Checkbox />} label="Kids" />
+                    </li>
+                    <li>
+                    <FormControlLabel className='w-100' control={<Checkbox />} label="Watched" />
+                    </li>
+                    <li>
+                    <FormControlLabel className='w-100' control={<Checkbox />} label="Gifts" />
+                    </li>
+                 </ul>
+                </div>
+            </div>
+
+            <div className='filterBox'>
+                <h6>FILTER BY PRICE</h6>
+                <RangeSlider value={value} onInput={setValue} min={100} max={60000} step={5} />
+                <div className='d-flex pt-2 pb-2 priceRange'>
+                    <span>From: <strong className='text-dark'>$: {value[0]}</strong></span>
+                    <span className='ml-auto'>To: <strong className='text-dark'>$: {value[1]}</strong></span>
+                </div>
+            </div>
+
+            <div className="filterBox">
+                <h6>PRODUCT STATUS</h6>
+                <div className='scroll'>
+                 <ul>
+                    <li>
+                    <FormControlLabel className='w-100' control={<Checkbox />} label="In Stock" />
+                    </li>
+                    <li>
+                    <FormControlLabel className='w-100' control={<Checkbox />} label="On sale" />
+                    </li>
+                 </ul>
+                </div>
+            </div>
+
+            <div className="filterBox">
+                <h6>BRANDS</h6>
+                <div className='scroll'>
+                 <ul>
+                    <li>
+                    <FormControlLabel className='w-100' control={<Checkbox />} label="LCWkiki" />
+                    </li>
+                    <li>
+                    <FormControlLabel className='w-100' control={<Checkbox />} label="FOLO" />
+                    </li>
+                    <li>
+                    <FormControlLabel className='w-100' control={<Checkbox />} label="Defacto" />
+                    </li>
+                    <li>
+                    <FormControlLabel className='w-100' control={<Checkbox />} label="Decathlon" />
+                    </li>
+                    <li>
+                    <FormControlLabel className='w-100' control={<Checkbox />} label="Hepsiburada" />
+                    </li>
+                    <li>
+                    <FormControlLabel className='w-100' control={<Checkbox />} label="N11" />
+                    </li>
+                 </ul>
+                </div>
+            </div>
+
+            <br/>
+            <Link to={"#"}>
+                <img src='https://klbtheme.com/bacola/wp-content/uploads/2021/05/sidebar-banner.gif' className='w-100' />
+            </Link>
         </div>
         </>
     )
